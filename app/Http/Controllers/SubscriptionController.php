3b00dcf55c18262f27a3b1982e116e6b8936316e
Jackie <dev@reactive.xyz>
2017-12-18 17:40:36 -0500

    Shit, forgot PHP with . concatenation

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -53,7 +53,7 @@ class SubscriptionController extends Controller
     		$csv->insertAll($subscriptions->toArray());
     	});
 
-    	$csv->output(strval(time())+'.csv');
+    	$csv->output(strval(time()).'.csv');
     	exit;
     }
 
